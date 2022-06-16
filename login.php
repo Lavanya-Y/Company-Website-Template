10179459b43590ab0e65724a5a6032ee05692575
Lavanya-Y <56256629+Lavanya-Y@users.noreply.github.com>
2022-06-16 15:04:50 +0530

    Files added

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+    <signup>
+
+        <div class="form-container">
+            <p class="login-text">Login with social media</p>
+            <div class="login-social">
+                <a href="#" class="facebook"><i class="fa fa-facebook"></i> Facebook</a>
+                <a href="#" class="twitter"><i class="fa fa-twitter"></i> Twitter</a>
+                <a href="#" class="google-plus"><i class="fa fa-google-plus"></i> Google Plus</a>
+                <a href="#" class="linkedin"><i class="fa fa-linkedin"></i> Linkedin</a>
+            </div>
+            <form class="login-email">
+                <p class="login-text">Login with email</p>
+                <div class="input-group">
+                    <input type="email" placeholder="Email" required>
+                </div>
+                <div class="input-group">
+                    <input type="password" placeholder="Password" required>
+                </div>
+                <div class="input-group">
+                    <button class="btn">Login</button>
+                </div>
+                <p class="login-reg-text">Don't have an account? <a href="signup.php">Register Here</a>.</p>
+            </form>
+        </div>
+    </signup>
+
+</body>
+
+</html>
---
+++ signup.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+    <signup>
+
+        <div class="form-container">
+            <form class="login-email">
+                <p class="login-text">REGISTER</p>
+                <div class="input-group">
+                    <input type="text" placeholder="Username" name="username" required>
+                </div>
+                <div class="input-group">
+                    <input type="email" placeholder="Email" name="email" required>
+                </div>
+                <div class="input-group">
+                    <input type="password" placeholder="Password" name="password" required>
+                </div>
+                <div class="input-group">
+                    <input type="password" placeholder="Confirm Password" name="cpassword" required>
+                </div>
+                <div class="input-group">
+                    <button class="btn">Register</button>
+                </div>
+                <p class="login-reg-text">Have an account? <a href="login.php">Login Here</a>.</p>
+            </form>
+        </div>
+    </signup>
+
+</body>
+
+</html>
